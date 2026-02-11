cee8199c94f1afa0417c146efcd85e1461fa10bb
DSM2 <fatec-dsm2@academico.intra>
2026-02-11 13:54:36 -0300

    Aula-

---
+++ js-aula-01-de-funcoes/variaveis/script.js
@@ -0,0 +1,20 @@
+// Comentario de linha
+
+/* comentario 
+de bloco */
+
+// VARIAVEIS PODEM SER DECLARADAS DE 3 FORMAS:
+
+var cidade = "Registro";
+var cidade = "Sete Barras";
+
+document.write(cidade);
+
+let nome = "diego";
+nome = "Maria";
+document.write("<br>");
+document.write(nome);
+
+const pais = "Brasil"
+pais ="Argentina"
+O Valor de uma constante nao pode ser alterado
